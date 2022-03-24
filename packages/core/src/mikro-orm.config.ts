353bb96a374b0e3f114ed51506f3428dde93a24b
Mass8326 <reyf.mag@gmail.com>
2022-03-23 23:39:06 -0400

    CI(CORE): Get migrations working with webpack

--- packages/core/src/mikro-orm.config.ts
+++ packages/core/src/mikro-orm.config.ts
@@ -1,6 +1,7 @@
 import type { BetterSqliteDriver } from "@mikro-orm/better-sqlite";
-import { resolve } from "path";
+import { resolve, basename } from "path";
 import { Options } from "@mikro-orm/core";
+import { Migration } from "@mikro-orm/migrations";
 import { SqlHighlighter } from "@mikro-orm/sql-highlighter";
 
 const dbName = process.env.STASH_DIR
@@ -8,11 +9,21 @@ const dbName = process.env.STASH_DIR
   : "dev.db";
 console.log(`Using ${dbName}`);
 
+// Webpack workaround to import all migrations from folder
+const context = require.context("../migrations", false, /\.ts$/);
+const migrationsList = context.keys().map((key) => {
+  const constructor = Object.values(context(key))[0] as new () => Migration;
+  return {
+    name: basename(key),
+    class: constructor,
+  };
+});
+
 const config: Options<BetterSqliteDriver> = {
   // Database
   dbName,
   type: "better-sqlite",
-  migrations: { path: "dist/migrations", pathTs: "src/migrations" },
+  migrations: { migrationsList },
   // Entities
   entities: ["./dist/**/*.entity.js"],
   entitiesTs: ["./src/**/*.entity.ts"],
