0d9019ccb77a6f374d2ac9be41358e9ec70344bf
Mass8326 <reyf.mag@gmail.com>
2022-04-14 00:52:44 -0400

    STYLE(UI): Create thumbnail grid layout

---
+++ packages/ui/src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script context="module">
+  import "modern-normalize/modern-normalize.css";
+</script>
+
+<slot />
+
+<style>
+  :global(body) {
+    margin: 0.5em;
+  }
+</style>
--- packages/ui/src/routes/index.svelte
+++ packages/ui/src/routes/index.svelte
@@ -1,7 +1,9 @@
-<script lang="ts">
+<script lang="ts" context="module">
   import type { TrpcSchema } from "dstash-core";
   import { createTRPCClient } from "@trpc/client";
+</script>
 
+<script lang="ts">
   type QueryAwaited<T extends keyof TrpcSchema["_def"]["queries"]> = Awaited<
     ReturnType<TrpcSchema["_def"]["queries"][T]["call"]>
   >;
@@ -27,6 +29,7 @@
     await init();
   }
 
+  /*
   const inputs = {} as Record<number, string>;
   async function addTag(id: number) {
     const result = await client.mutation("entry.tag-add", {
@@ -37,22 +40,42 @@
     const index = entries.findIndex((entry) => entry.id === id);
     entries[index] = { ...result, tags: await client.query("entry.tags", id) };
   }
+  */
+
+  let sizeArr = [100, 150, 200, 300, 450, 600];
+  let sizeInd = 2;
+  function bigger() {
+    if (sizeInd < sizeArr.length - 1) sizeInd++;
+  }
+  function smaller() {
+    if (sizeInd > 0) sizeInd--;
+  }
 </script>
 
-<h1>Welcome to SvelteKit</h1>
 <p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  Actions:
+  <button on:click={consume}>Consume</button>
 </p>
-<button on:click={consume}>Consume</button>
+<p>
+  Icon Size:
+  <button on:click={smaller}>Smaller</button>
+  <button on:click={bigger}>Bigger</button>
+</p>
 {#await init()}
   <p>Loading entries...</p>
 {:then}
   {#if entries.length === 0}
     <p>No entries found!</p>
   {/if}
-  <ul>
+  <div class="list" style={`--icon-size:${sizeArr[sizeInd]}px`}>
     {#each entries as entry}
-      <li>
+      <a href={`item/${entry.hash}`} class="item">
+        <img
+          class="bg"
+          src={`http://localhost:4000/entry/${entry.id}`}
+          alt=""
+        />
+        <!--         
         <p>{entry.id} - {entry.hash}</p>
         <ul>
           {#if !entry.tags}
@@ -68,14 +91,67 @@
             {inputs[entry.id]}
           </li>
         </ul>
-        <img src={`http://localhost:4000/entry/${entry.id}`} alt="" />
-      </li>
+        -->
+        <img
+          class="thumbnail"
+          src={`http://localhost:4000/entry/${entry.id}`}
+          alt=""
+        />
+        <span class="label">{entry.id}</span>
+      </a>
     {/each}
-  </ul>
+  </div>
 {/await}
 
-<style>
-  img {
-    max-height: 240px;
+<style lang="scss">
+  .list {
+    display: grid;
+    grid-template-columns: repeat(
+      auto-fit,
+      minmax(var(--icon-size, 240px), 1fr)
+    );
+  }
+  .item {
+    margin: 0.5em;
+    border: 1px solid #ddd;
+    aspect-ratio: 1/1;
+
+    // Background
+    position: relative;
+    overflow: hidden;
+    &:hover {
+      background-color: #eee9;
+      border: 1px solid #999;
+    }
+    .bg {
+      position: absolute;
+      height: calc(100% + 2em);
+      width: calc(100% + 2em);
+      filter: blur(1em);
+      opacity: 0.5;
+      z-index: -1;
+    }
+    &:hover .label {
+      visibility: visible;
+    }
+    .label {
+      visibility: hidden;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      margin: 0.25em;
+      border-radius: 0.25em;
+      padding: 0.25em;
+      background-color: #fff;
+    }
+
+    // Flex children
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .thumbnail {
+      max-height: 100%;
+      max-width: 100%;
+    }
   }
 </style>
